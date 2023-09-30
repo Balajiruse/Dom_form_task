17d123ac636dab209ee372eb5313213df7a67dc2
Balaji <131993048+Balajiruse@users.noreply.github.com>
2023-09-30 23:18:25 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,35 @@
+const Form = document.getElementById("details")
+const Tbale=document.querySelector(".table")
+
+
+const sumbit = document.getElementById("button")
+const btnclear =document.querySelector(".btnclear")
+
+sumbit.addEventListener("click", ()=>{
+    const First =document.getElementById("firstname").value;
+    const last =document.getElementById("lastname").value;
+    const gende =document.getElementById("gender").value;
+    const state =document.getElementById("state").value;
+    const pincode =document.getElementById("pincode").value;
+
+    const firstRow=document.getElementById("first")
+const secondRow=document.getElementById("second")
+const thirdRow=document.getElementById("third")
+const forthRow=document.getElementById("forth")
+const FiveRow=document.getElementById("five")
+
+firstRow.innerText=First
+secondRow.innerText=last
+thirdRow.innerText=gende
+forthRow.innerText=state
+FiveRow.innerText=pincode
+
+
+})
+
+
+
+
+
+
+
